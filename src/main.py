a8e2f1a35cbfa14e6179dc041523f15a153001f3
Robert Scheinpflug <ro.scheinpflug@ostfalia.de>
2016-01-31 17:07:01 +0100

    fetch only first 30 stories

--- src/main.py
+++ src/main.py
@@ -9,11 +9,17 @@ firebase = firebase.FirebaseApplication('https://hacker-news.firebaseio.com', No
 def getNewstories():
     newstories = firebase.get('/v0/newstories', None)
     newstory = None
+    i = 0
 
     for newstory in newstories:
         newstory = str(newstory)
 
         story = firebase.get_async('/v0/item/'+newstory, None, callback=eventNewStory)
+
+        i += 1
+
+        if i > 30:
+            break
 
 
 def eventNewStory(response):
